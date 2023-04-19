c81b3c70244ff871480f8856919f41e422aafad1
Thomas SOUFFLET <thomassoufflet16@gmail.com>
2023-04-19 16:11:40 +0200

    use data.txt as train data and rename result file

--- compareResult.py
+++ compareResult.py
@@ -1,10 +1,13 @@
 #Full path
-f1 = open(r"/Users/thomas/Library/CloudStorage/OneDrive-DeVinci/ESILV/A3/S6/Data et IA/KNN/resultKNN.txt","r")
+f1 = open(r"/Users/thomas/Library/CloudStorage/OneDrive-DeVinci/ESILV/A3/S6/Data et IA/SOUFFLETthomas-VARACCAmaxime-TDi.txt","r")
 #Relative path
-f2 = open(r"KNN/resultKNN.txt","r")
+f2 = open(r"/Users/thomas/Downloads/PlaideauAnna-BenfetitaIlyana-TDQ.txt","r")
 
 linesf1 = f1.readlines()
 linesf2 = f2.readlines()
+
+#print(len(linesf1))
+#print(len(linesf2))
 
 sameLength = True if len(linesf1) == len(linesf2) else False
 
@@ -15,5 +18,6 @@ if sameLength:
             similaire +=1
     
     print("Le taux de similitude est de : ", similaire/len(linesf1)*100, "%" )
+    print(4000-similaire, "erreurs")
 else:
     print("Les fichiers ne sont pas de la même taille")
--- k-nn.py
+++ k-nn.py
@@ -14,6 +14,13 @@ def distance(dataTest,a,b,c,d,e,f,g):
     pointa= np.array((a,b,c,d,e,f,g))
     return np.linalg.norm(dataTest-pointa)
 
+#Calcule la distance normalisée entre le point de test et un point d'entrainement de coordonnées (a,b,c,d,e,f,g)
+def distanceNormalisee(dataTest,a,b,c,d,e,f,g):
+    #print("dataTest : ", dataTest)
+    #print("a,b,c,d,e,f,g : ", a,b,c,d,e,f,g)
+    pointa= np.array((a,b,c,d,e,f,g))
+    return (np.linalg.norm(dataTest-pointa)-np.mean(dataTest-pointa))/np.std(dataTest-pointa)
+
 
 def knn(dataTest,k,fdest,df_train):
     #initialise la liste des distances
@@ -22,6 +29,7 @@ def knn(dataTest,k,fdest,df_train):
     #Parcourt le dataframe d'entrainement et calcule la distance entre le point de test et chaque point d'entrainement
     for index, row in df_train.iterrows():
         dist = distance(dataTest, row['a'], row['b'],row['c'], row['d'], row['e'], row['f'], row['g'])
+        #dist = distanceNormalisee(dataTest, row['a'], row['b'],row['c'], row['d'], row['e'], row['f'], row['g'])
         distances.append((dist,row['label']))
     #Trie la liste des distances
     distances.sort()
@@ -38,14 +46,13 @@ def knn(dataTest,k,fdest,df_train):
     #Calcul du label le plus présent dans le dictionnaire de résultat
     label = max(result, key=lambda k: result[k])
     
-    #calcul le label le plus présent dans le dictionnaire de résultat
-    
 
     
     #Ecris le label dans le fichier de résultat
     fdest.write(str(label)+'\n')
 
-
+#Fonction utilisé lorsque un seul fichier de données est fourni. Il sera divisé en deux : 80% pour l'entrainement et 20% pour le test
+#Nous pouvons donc calculer la précision des résultats
 def main_data_divisied(df,fdest,k):  
         #Mélange les données
         df = df.sample(frac=1).reset_index(drop=True)
@@ -62,8 +69,8 @@ def main_data_divisied(df,fdest,k):
 
         fdest.close()
 
-        #Calcul de la précision des résultats stockés dans resultKNN.txt en fonction du label donné dans df_try et de la matrice de confusion
-        f = open(r"resultKNN.txt","r")
+        #Calcul de la précision des résultats stockés dans SOUFFLETthomas-VARACCAmaxime-TDi.txt en fonction du label donné dans df_try et de la matrice de confusion
+        f = open(r"SOUFFLETthomas-VARACCAmaxime-TDi.txt","r")
         lines = f.readlines()
         matrice = np.zeros((len(df_train['label'].unique()),len(df_train['label'].unique())))
         indexLines = 0
@@ -81,14 +88,29 @@ def main_data_divisied(df,fdest,k):
 
 def main():
     #Ouvre le fichier dans lequel on va stocker les résultats
-    fdest=open(r"resultKNN.txt","w")
+    fdest=open(r"SOUFFLETthomas-VARACCAmaxime-TDi.txt","w")
+    
     #Récupère le nombre de voisins k voulu
+    #Le k le plus optimal est 5
     k = input("Enter the k that you want :")
 
+
+    df_train=pd.read_csv("KNN/data.txt", names=["a","b","c","d","e","f","g","label"], sep=";")
+    df_try=pd.read_csv("KNN/finalTest.txt", names=["a","b","c","d","e","f","g"], sep=";")
+    for index, row in df_try.iterrows():
+            dataTest = np.array((row['a'], row['b'],row['c'], row['d'], row['e'], row['f'], row['g']))
+            knn(dataTest,k,fdest,df_train)
+    fdest.close()
+    
+    """
+    Cette partie du code permettait de tester le programme avec des données fournies par l'utilisateur rentrées en argument dans la console
+    Par exemple, python3 KNN.py data.txt preTest.txt pour prendre data.txt pour l'entrainement et preTest.txt pour le test
+    Elle n'est plus réellement utile pour le code final qui prend data.txt et finalTest.txt
+    
     #Si le nombre d'arguments est égal à 3, alors on prend les données de test et les données d'entrainement
     if len(sys.argv) == 3:
-        df_train=pd.read_csv(sys.argv[1], names=["a","b","c","d","e","f","g","label"])
-        df_try=pd.read_csv(sys.argv[2], names=["a","b","c","d","e","f","g","label"])
+        df_train=pd.read_csv(sys.argv[1], names=["a","b","c","d","e","f","g","label"], sep=";")
+        df_try=pd.read_csv(sys.argv[2], names=["a","b","c","d","e","f","g","label"], sep=";")
         for index, row in df_try.iterrows():
             dataTest = np.array((row['a'], row['b'],row['c'], row['d'], row['e'], row['f'], row['g']))
             knn(dataTest,k,fdest,df_train)
@@ -99,11 +121,21 @@ def main():
         df=pd.read_csv(sys.argv[1], names=["a","b","c","d","e","f","g","label"], sep=";")
         main_data_divisied(df,fdest,k)
         
-    #Si rien n'est donnée en argument, on prend les données data.txt    
+    #Si rien n'est donnée en argument, on prend les données data.txt (simplifie les tests du code sans avoir à changer les arguments) 
     else: 
         df=pd.read_csv(r"data.txt", names=["a","b","c","d","e","f","g","label"], sep=";")
-        main_data_divisied(df,fdest,k)
-             
+        
+        #Permet en entrant aucune valeur de k de tester pour k allant de 1 à 9 pour voir lequel donne le meilleur résultat
+        if k == "":
+            for k in range(1,10):
+                df=pd.read_csv(r"data.txt", names=["a","b","c","d","e","f","g","label"], sep=";")
+                print("k = ", k)
+                main_data_divisied(df,fdest,k)
+                fdest=open(r"SOUFFLETthomas-VARACCAmaxime-TDi.txt","w")
+
+        else:
+            main_data_divisied(df,fdest,k)
+    """
 
 if __name__ == "__main__":
     main()
